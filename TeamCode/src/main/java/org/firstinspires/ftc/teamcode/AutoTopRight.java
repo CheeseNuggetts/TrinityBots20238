8c2d6a820cf0bd5f6031b6dbd4a4214f31d1df17
CheeseNuggetts <yc20190101@gmail.com>
2023-11-26 13:18:49 -0600

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTopRight.java
@@ -0,0 +1,133 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="AutoTopRight", group="Robot")
+//@Disabled
+public class AutoTopRight extends LinearOpMode {
+    private DcMotor frontLeftMotor = null;
+    private DcMotor backLeftMotor = null;
+    private DcMotor frontRightMotor = null;
+    private DcMotor backRightMotor = null;
+    private ElapsedTime runtime=new ElapsedTime();
+    private Servo claw = null;
+    private Servo clarm = null;
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    @Override
+    public void runOpMode() {
+        frontLeftMotor = hardwareMap.dcMotor.get("leftfront");
+        backLeftMotor = hardwareMap.dcMotor.get("leftback");
+        frontRightMotor = hardwareMap.dcMotor.get("rightfront");
+        backRightMotor = hardwareMap.dcMotor.get("rightback");
+
+        claw=hardwareMap.get(Servo.class, "claw");
+        clarm=hardwareMap.get(Servo.class, "clarm");
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        claw.setPosition(0);
+        clarm.setPosition(1);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        waitForStart();
+
+        //1 tile is 9
+        encoderDrive(0.6, 18.5, 18.5, 69.420);
+        encoderDrive(0.5, 8.75, -8.75, 69.420);
+        encoderDrive(0.6, 16.5, 16.5, 69.420);
+        //This is the only way I could make it work, and I have no idea why. If you can, make this better.
+
+        clarm.setPosition(.52);
+        sleep(500);
+        for (double i=0.9; i<=1; i+=0.1) {
+            claw.setPosition(i);
+            sleep(250);
+        }
+    }
+
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newFrontLeftTarget;
+        int newFrontRightTarget;
+        int newBackLeftTarget;
+        int newBackRightTarget;
+
+        // Ensure that the OpMode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newFrontLeftTarget = frontLeftMotor.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newFrontRightTarget = frontRightMotor.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            newBackLeftTarget=backLeftMotor.getCurrentPosition()+(int)(leftInches*COUNTS_PER_INCH);
+            newBackRightTarget=backRightMotor.getCurrentPosition()+(int)(rightInches*COUNTS_PER_INCH);
+            frontLeftMotor.setTargetPosition(newFrontLeftTarget);
+            frontRightMotor.setTargetPosition(newFrontRightTarget);
+            backLeftMotor.setTargetPosition(newBackLeftTarget);
+            backRightMotor.setTargetPosition(newBackRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            frontLeftMotor.setPower(Math.abs(speed));
+            frontRightMotor.setPower(Math.abs(speed));
+            backLeftMotor.setPower(Math.abs(speed));
+            backRightMotor.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (frontLeftMotor.isBusy() && frontRightMotor.isBusy()) && backLeftMotor.isBusy() && backRightMotor.isBusy()) {
+                telemetry.addData("Running to",
+                        " %7d :%7d",
+                        newFrontLeftTarget,
+                        newFrontRightTarget,
+                        newBackLeftTarget,
+                        newBackRightTarget);
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            frontLeftMotor.setPower(-1);
+            frontRightMotor.setPower(-1);
+            backLeftMotor.setPower(-1);
+            backRightMotor.setPower(-1);
+            sleep(100);
+            frontLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            backLeftMotor.setPower(0);
+            backRightMotor.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            sleep(250);   // optional pause after each move.
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -0,0 +1,233 @@
+package org.firstinspires.ftc.teamcode;
+//bruh
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+public class MecanumTeleOp extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("leftfront");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("leftback");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("rightfront");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("rightback");
+        //DcMotor fourstage = hardwareMap.dcMotor.get("fourstage");
+        DcMotor twostage = hardwareMap.dcMotor.get("twostage");
+        //DcMotor plane = hardwareMap.dcMotor.get("plane");
+
+        //Servos
+        Servo out = hardwareMap.get(Servo.class, "out");
+        Servo claw = hardwareMap.get(Servo.class, "claw");
+        Servo clarm = hardwareMap.get(Servo.class, "clarm");
+        Servo plane = hardwareMap.get(Servo.class, "plane");
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //Reverse viperslide motors
+
+        //fourstage.setDirection(DcMotorSimple.Direction.REVERSE);
+        //twostage.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //Brake
+        //fourstage.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        twostage.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //Set position
+        //fourstage.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //fourstage.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        twostage.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //twostage.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        twostage.setTargetPosition(0);
+        twostage.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //fourstage.setTargetPosition(0);
+        //fourstage.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        out.setPosition(.7);
+        clarm.setPosition(1);
+        claw.setPosition(0.95);
+        plane.setPosition(0);
+
+        //Speed
+        Boolean normalmode = true;
+        Boolean turbomode = false;
+        Boolean slowmode = false;
+        String mode = "normalmode";
+
+
+        waitForStart();
+
+        if (isStopRequested()) {
+            out.setPosition(0);
+        }
+
+        while (opModeIsActive()) {
+
+            out.getPosition();
+            telemetry.addLine("sussy bakea");
+            telemetry.addData("out pos", out.getPosition());
+            telemetry.addData("clarm pos", clarm.getPosition());
+            telemetry.addData("clawpos", claw.getPosition());
+            telemetry.addData("planepos", plane.getPosition());
+
+            telemetry.update();
+
+            if (gamepad2.left_bumper) {
+                out.setPosition(.7);
+            }
+            else if (gamepad2.right_bumper) {
+                out.setPosition(0.1);
+            }
+
+            if (gamepad2.left_stick_button) {
+                claw.setPosition(0.95);
+            }
+            if (gamepad2.right_stick_button) {
+                claw.setPosition(0.5);
+            }
+
+            if (gamepad2.y) {
+                clarm.setPosition(1);
+            }
+            if (gamepad2.x) {
+                clarm.setPosition(0.52);
+            }
+            if (gamepad2.a) {
+                clarm.setPosition(0.462);
+            }
+            if (gamepad2.dpad_left) {
+                plane.setPosition(0);
+            }
+            if (gamepad2.dpad_right) {
+                plane.setPosition(1);
+            }
+
+
+            //More encoder stuff
+            double CPR = 537.7;
+
+            // Get the current position of the motor ticks
+            /*int fourpos = fourstage.getCurrentPosition();
+            double fourrev = fourpos / CPR;
+            double fourang = fourrev * 360;
+            double fourangnorm = fourang % 360;*/
+
+            int twopos = twostage.getCurrentPosition();
+            double tworev = twopos / CPR;
+            double twoang = tworev * 360;
+            double twoangnorm = twoang % 360;
+
+
+            if (gamepad2.dpad_up) {
+                twostage.setTargetPosition(2200);
+                twostage.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                twostage.setPower(.5);
+            }
+            if (gamepad2.dpad_down) {
+                twostage.setTargetPosition(0);
+                twostage.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                twostage.setPower(.5);
+            }
+
+            /*if (gamepad2.dpad_left) {
+                fourstage.setTargetPosition(3300);
+                fourstage.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                fourstage.setPower(1);
+            }
+            if (gamepad2.dpad_right) {
+                fourstage.setTargetPosition(0);
+                fourstage.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                fourstage.setPower(1);
+            }*/
+
+            /*if (gamepad1.right_bumper) {
+                plane.setPower(1);
+            } else {
+                plane.setPower(0);
+            }*/
+
+
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            if (gamepad1.left_bumper) {
+                slowmode = true;
+                turbomode = false;
+                normalmode = false;
+                mode = "slowmode";
+            }
+
+            if (gamepad1.right_stick_button) {
+                slowmode = false;
+                turbomode = false;
+                normalmode = true;
+                mode = "normalmode";
+            }
+
+            if (gamepad1.right_bumper) {
+                slowmode = false;
+                turbomode = true;
+                normalmode = false;
+                mode = "turbomode";
+            }
+
+            telemetry.addData("mode", mode);
+
+            if (normalmode == true) {
+                frontLeftMotor.setPower(frontLeftPower / 2);
+                backLeftMotor.setPower(backLeftPower / 2);
+                frontRightMotor.setPower(frontRightPower / 2);
+                backRightMotor.setPower(backRightPower / 2);
+            }
+            if (turbomode == true) {
+                frontLeftMotor.setPower(frontLeftPower*.8);
+                backLeftMotor.setPower(backLeftPower*.8);
+                frontRightMotor.setPower(frontRightPower*.8);
+                backRightMotor.setPower(backRightPower*.8);
+            }
+            if (slowmode == true) {
+                frontLeftMotor.setPower(frontLeftPower / 5);
+                backLeftMotor.setPower(backLeftPower / 5);
+                frontRightMotor.setPower(frontRightPower / 5);
+                backRightMotor.setPower(backRightPower / 5);
+            }
+        }
+    }
+
+
+
+
+
+
+}
